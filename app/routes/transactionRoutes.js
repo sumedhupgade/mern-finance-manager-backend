045f5aaf5cb0d3a9aa52a931cc62834b423ddb78
ssu.techture@gmail.com <ssu.techture@gmail.com>
2024-09-01 12:32:39 +0530

    code refactored

--- app/routes/transactionRoutes.js
+++ app/routes/transactionRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const Transaction = require('../models/Transaction');
+const Transaction = require('../Transaction');
 
 // @route   POST /api/transactions
 // @desc    Create a new transaction
--- server.js
+++ server.js
@@ -3,38 +3,37 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const transactionRoutes = require('./routes/transactionRoutes');
+const transactionRoutes = require("./app/models/routes/transactionRoutes");
 
 // Connect to MongoDB
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, {
+mongoose
+  .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
-app.use('/api/transactions', transactionRoutes);
+app.use("/api/transactions", transactionRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello, MERN Stack!');
+app.get("/", (req, res) => {
+  res.send("Hello, MERN Stack!");
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // sumedh225109 OrL9s7VnRpjNXY2A
-
